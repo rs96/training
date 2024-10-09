a6c2fe0cb3effd2c05f01f8f0663551a85992e91
Rob Shipley <shipleymrobert@gmail.com>
2024-10-09 22:03:19 +0100

    set up app code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => (
+  <div className="card">
+    <button onClick={() => {}}>count is lol broke</button>
+    <p>
+      Edit <code>src/App.tsx</code> and save to test HMR
+    </p>
+  </div>
+);
+
+export default App;
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,4 @@
-import { Session } from "./types";
+import { Session } from "../types";
 
 const WINTER: Session[] = [
   {
