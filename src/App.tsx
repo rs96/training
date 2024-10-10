06734166095946a3a3e8bf3fb89bc4ee4b49a778
Rob Shipley <shipleymrobert@gmail.com>
2024-10-10 08:58:10 +0100

    starting table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import "./App.css";
+import styles from "./app.module.css";
+import Table from "./components/table/Table";
 
 const App = () => (
-  <div className="card">
-    <button onClick={() => {}}>count is lol broke</button>
-    <p>
-      Edit <code>src/App.tsx</code> and save to test HMR
-    </p>
-  </div>
+  <>
+    <div className={styles.title}>Training Diary</div>
+    <div className={styles.mainColumn}>
+      <Table
+        headers={["This", "Is", "A", "Test"]}
+        rows={[
+          [0, 1, 2, 3],
+          [9, 8, 7, 6],
+        ]}
+      />
+    </div>
+    <div className="card">
+      <button onClick={() => {}}>count is lol broke</button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  </>
 );
 
 export default App;
